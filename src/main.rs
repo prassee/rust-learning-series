30e695a0e4ae7905fdc2a4cea963e19c3ddf4dcf
Prasanna <Prasanna>
2020-10-11 09:36:37 +0530

    updated code for ownership

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-pub(crate) fn main() {}
+pub(crate) fn main() {
+    let s = String::from("hello");
+    println!("{}", s);
+    let s2 = s.clone();
+    println!("{}", s2);
+    println!("{}", s);
+    let vec = Vec::<i32>::new();
+    let mut vec1 = fill_vec();
+
+    println!("{} has length {} content `{:?}`", "vec0", vec.len(), vec);
+    vec1.push(23);
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn fill_vec() -> Vec<i32> {
+    let mut vec = Vec::new();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+    vec
+}
