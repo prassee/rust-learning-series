2a0295ac539064109d5ef03ccf4eae5d391e7803
Prasanna <Prasanna>
2020-10-20 06:25:54 +0530

    using use keyword

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate rust_yt_series;
 
+use rust_yt_series::{api, publisher};
+
 pub(crate) fn main() {
+    publisher::connect();
+    api::api_call();
     rust_yt_series::publisher::connect();
     rust_yt_series::api::api_call();
 }
